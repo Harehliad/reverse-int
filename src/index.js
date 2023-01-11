b5c4287172563102b9ce6a68826399a2487d6faf
Harehliad Mikhail <harehliadml@gmail.com>
2023-01-11 16:01:59 +0300

    feat: created a program

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (nuber) {
+    if (nuber < 0){
+        nuber = -nuber
+    }
+    let revers = ''
+    for(let i = nuber.toString().length ; i >= 1; i--){
+        revers = revers + nuber.toString()[i-1];
+        }
+        return revers;
 }
